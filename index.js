8add14aa1b8f10f35c17700b4ba2c9c5c426e0c3
kiranwankhade <kiranwankhade7738@gamil.com>
2023-02-23 17:11:28 +0530

    fullAss

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ require('dotenv').config()
 const app = express();
 app.use(express.json());
 
-// var cors = require('cors')
-// app.use(cors())
+var cors = require('cors')
+app.use(cors())
 
 
 app.get("/",(req,res)=>{
